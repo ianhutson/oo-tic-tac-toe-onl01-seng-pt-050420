22770baa1b2356213085a7883e08a8173da8d2ea
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-26 20:53:32 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,42 @@
+class TicTacToe
+  def initialize
+  @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+  
+  WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+ ]
+ 
+ def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+ end
+ e1
+ def input_to_index(input)
+   input.to_i - 1
+ end
+ 
+  def move(index, xo)
+    @board[index] = xo
+  end
+  
+  def position_taken?(index)
+    @board[index] != " "
+  end
+  
+  def valid_move?(index)
+    !position_taken?(index) && index.between?(0,8)
+  end
+  
+  
+end
